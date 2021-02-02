a057a75378db3429865fa7eb55582b119570a746
Aaron Blok <533677@student.glu.nl>
2021-02-02 14:24:28 +0100

    Added player dash

--- Shadows Of The East/Assets/Scripts/PlayerMovement.cs
+++ Shadows Of The East/Assets/Scripts/PlayerMovement.cs
@@ -11,18 +11,55 @@ public class PlayerMovement : MonoBehaviour
 
     private float PlayerPositionX;
     [SerializeField] private float speed = 20f;
+    [Header("Dash settings")]
+    [SerializeField] private float dashDistance = 20f;
+    [SerializeField] private float dashCooldown = 4f;
+    private float dashCooldownTimer = 0;
+    private float lastDirection = 1;
 
     private void Update()
     {
         if (Input.anyKey)
         {
+            PlayerPositionX = transform.position.x;
             UpdatePlayerMovement();
+            UpdateDash();
+            transform.position = new Vector2(PlayerPositionX, transform.position.y);
         }
+
+        UpdateTimers();
+    }   
+
+    private void UpdateTimers()
+    {
+        dashCooldownTimer -= Time.deltaTime;
     }
 
+    private void UpdateDash()
+    {   
+        if (dashCooldownTimer <= 0 && Input.GetAxis("Dash") > 0)
+        {
+            PlayerPositionX += lastDirection * dashDistance;
+
+            dashCooldownTimer = dashCooldown;
+        }
+    }
     private void UpdatePlayerMovement()
     {
-        PlayerPositionX += Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        transform.position = new Vector2(PlayerPositionX, transform.position.y);
+        float horizontal = Input.GetAxis("Horizontal");
+
+        if (horizontal != 0)
+        {
+            if (horizontal > 0)
+            {
+                lastDirection = 1;
+            }
+            else
+            {
+                lastDirection = -1;
+            }
+        }
+
+        PlayerPositionX += horizontal * Time.deltaTime * speed;
     }
 }
