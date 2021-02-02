fb49f520c6c7a346596c8ebc9b1e4a5f73104b73
Callep3 <42645914+Callep3@users.noreply.github.com>
2021-02-02 13:11:17 +0100

    Framework

--- Shadows Of The East/Assets/Scripts/PlayerMovement.cs
+++ Shadows Of The East/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Inputs
+    //Player Move
+    //Player Dash
+
+    private float PlayerPositionX;
+    [SerializeField] private float speed = 20f;
+
+    private void Update()
     {
-        
+        if (Input.anyKey)
+        {
+            UpdatePlayerMovement();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdatePlayerMovement()
     {
-        
+        PlayerPositionX += Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        transform.position = new Vector2(PlayerPositionX, transform.position.y);
     }
 }
