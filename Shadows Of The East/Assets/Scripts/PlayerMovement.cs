7d9554afcfff800afcd5e2ab3d56cf14922b9106
Aaron Blok <533677@student.glu.nl>
2021-02-02 14:46:57 +0100

    Added player jumping

--- Shadows Of The East/Assets/Scripts/PlayerMovement.cs
+++ Shadows Of The East/Assets/Scripts/PlayerMovement.cs
@@ -10,29 +10,60 @@ public class PlayerMovement : MonoBehaviour
     //Player Dash
 
     private float PlayerPositionX;
+    private Rigidbody2D rigidBody;
+
+    [Header("Movement settings")]
     [SerializeField] private float speed = 20f;
     [Header("Dash settings")]
     [SerializeField] private float dashDistance = 20f;
     [SerializeField] private float dashCooldown = 4f;
     private float dashCooldownTimer = 0;
     private float lastDirection = 1;
+    [Header("Jump settings")]
+    [SerializeField] private float jumpForce = 2f;
+    [SerializeField] private LayerMask groundLayers;
+    private float jumpBuffer;
 
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
     private void Update()
     {
+        PlayerPositionX = transform.position.x;
+
         if (Input.anyKey)
         {
-            PlayerPositionX = transform.position.x;
             UpdatePlayerMovement();
             UpdateDash();
-            transform.position = new Vector2(PlayerPositionX, transform.position.y);
         }
+        UpdateJump();
 
         UpdateTimers();
+        transform.position = new Vector2(PlayerPositionX, transform.position.y);
     }   
 
     private void UpdateTimers()
     {
         dashCooldownTimer -= Time.deltaTime;
+        jumpBuffer -= Time.deltaTime;
+    }
+
+    private void UpdateJump()
+    {
+        if (Input.GetAxis("Jump") > 0 )
+        {
+            jumpBuffer = 0.2f;
+        }
+
+        if (jumpBuffer > 0)
+        {
+            if (Physics2D.Raycast(transform.position, Vector3.down, 0.7f, groundLayers))
+            {
+                jumpBuffer = 0;
+                rigidBody.velocity = Vector2.up * jumpForce;
+            }
+        }
     }
 
     private void UpdateDash()
@@ -62,4 +93,6 @@ public class PlayerMovement : MonoBehaviour
 
         PlayerPositionX += horizontal * Time.deltaTime * speed;
     }
+
+    
 }
