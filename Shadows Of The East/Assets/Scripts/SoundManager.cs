1b952d98d5d970bfd1de00b5aef5890f970618dd
Aaron Blok <533677@student.glu.nl>
2021-02-03 16:48:49 +0100

    Added sounds to zombies

---
+++ Shadows Of The East/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private int maxAmountOfMoans = 5;
+    [SerializeField] List<AudioSource> moanSources = new List<AudioSource>();
+    public static SoundManager Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+            Destroy(gameObject);
+    }
+
+
+    public bool AllowedToPlayMoan(AudioSource moan)
+    {
+        UpdateMoans();
+        if (moanSources.Count >= maxAmountOfMoans)
+        {
+            return false;
+        }
+        else
+        {
+            moanSources.Add(moan);
+            return true;
+        }
+    }
+
+    private void UpdateMoans()
+    {
+        List<AudioSource> toRemoveSources = new List<AudioSource>();
+        foreach (AudioSource source in moanSources)
+        {
+            if (source != null)
+            {
+                if (!source.isPlaying)
+                {
+                    toRemoveSources.Add(source);
+                }
+            }
+            else
+            {
+                toRemoveSources.Add(source);
+            }
+        }
+
+        foreach (AudioSource source in toRemoveSources)
+        {
+            moanSources.Remove(source);
+        }
+    }
+
+}
